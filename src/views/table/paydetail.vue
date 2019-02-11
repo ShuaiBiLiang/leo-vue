962f2c6d015e90d24dbe9155b8b5e821033d5278
LiLiang <you@example.com>
2019-02-11 17:32:21 +0800

    asdfsadf

--- src/views/table/paydetail.vue
+++ src/views/table/paydetail.vue
@@ -23,11 +23,11 @@
       status: "Active"
       volume: "100" -->
 
-      <el-table-column prop="cost" label="cost" width="80" show-overflow-tooltip/>
-      <el-table-column prop="dateTime" label="dateTime" min-width="160" show-overflow-tooltip/>
-      <el-table-column prop="id" label="id" min-width="130" show-overflow-tooltip/>
-      <el-table-column prop="orderType" label="orderType" min-width="130" show-overflow-tooltip/>
-      <el-table-column prop="price" label="price" width="60" show-overflow-tooltip/>
+      <el-table-column prop="cost" label="cost" width="100" show-overflow-tooltip/>
+      <el-table-column prop="dateTime" label="dateTime" min-width="100" show-overflow-tooltip/>
+      <el-table-column prop="id" label="id" min-width="60" show-overflow-tooltip/>
+      <el-table-column prop="orderType" label="orderType" min-width="60" show-overflow-tooltip/>
+      <el-table-column prop="price" label="price" width="100" show-overflow-tooltip/>
       <el-table-column prop="status" label="status" width="60" show-overflow-tooltip/>
       <el-table-column prop="volume" label="volume" width="60" align="right" show-overflow-tooltip/>
     </el-table>
